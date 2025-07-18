20f54d33a12af63c2efc9da548555c71e532ecc2
Nick Lima <nick.lima.wp@gmail.com>
2025-07-18 16:10:17 -0300

    Update ReactDOM import and refactor rendering method for compatibility with React 18

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,11 @@
 import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client"; // <-- update import
 
 import App from "./containers/App";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
+ReactDOM.createRoot(rootElement).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-  rootElement
+  </StrictMode>
 );
